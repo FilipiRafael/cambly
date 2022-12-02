94c3cc9a329cba584d1a9cb67817d236cc5901a1
Filipi Rafael <filipirafael.123@gmail.com>
2022-12-01 22:36:11 -0300

    Add new updates

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 export const Banner = styled.div`
-  width: 400px;
-  height: 400px;
-  background-color: blue;
+  width: 100vw;
+  height: 320px;
+  background-image: url('/images/yellow-banner.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
