43ca34c6d029d350ddbac776bd3d577aab532be3
Filipi Rafael <filipirafael.123@gmail.com>
2022-12-01 22:26:53 -0300

    Add new updates

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import { Banner } from './styles';
+
+export const Home = () => {
+  return (
+    <Banner />
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: blue;
+`;
